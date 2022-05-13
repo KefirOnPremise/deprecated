f0648bf0ce76e9c370534cf3f7c69dc30f794a32
Adam Kandur <kefironpremise@gmail.com>
2022-05-13 18:20:22 +0300

    wip

--- kefir/pkgs/go.scm
+++ kefir/pkgs/go.scm
@@ -4,9 +4,9 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system go))
 
-(define-public emacs-TestDaemon
+(define-public go-TestDaemon
   (package
-    (name "emacs-TestDaemon")
+    (name "go-TestDaemon")
     (version "initial_program")
     (source
      (origin
@@ -17,7 +17,7 @@
        (file-name (git-file-name name version))
        (sha256
 	(base32 "0sly92qjk8jj6z3lpnk76jf9204vdfq7b8jwvkksx9pmd7d0ag15"))))
-    (build-system emacs-build-system)
+    (build-system go-build-system)
     (home-page "https://github.com/KefirOnPremise/TestDaemon")
     (synopsis "")
     (description
