7fc7fd28d5b534a6ac3c375936a0da6a3eb509ec
Adam Kandur <>
2022-06-28 17:33:43 +0300

    wip

---
+++ kefir/misc/base.scm
@@ -0,0 +1,9 @@
+(define-module (kefir misc base)
+  #:export (my-system-packages))
+
+(define my-system-packages
+  '("emacs"
+    "emacs-exwm"
+    "emacs-magit"
+    "emacs-kefir-stuff"
+    "nss-certs"))
