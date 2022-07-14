7118734bf9f722d983c2b3d9742cf9d5c7b7914f
Adam Kandur <>
2022-07-14 12:01:20 +0300

    wip

--- kefir/pkgs/emacs.scm
+++ kefir/pkgs/emacs.scm
@@ -40,7 +40,7 @@
 	     (commit commit)))
        (file-name (git-file-name name version))
        (sha256
-	(base32 "1q2wn1w79gm8pc38x74vi30yyr0b00j9aaaqq13s2pcgbm0ny68d"))))
+	(base32 "0c9lknrhwhi4v3qnnzmy0k24d8pr3d3mpxi9xzcxq97f5w1x4fsk"))))
      (build-system emacs-build-system)
      (arguments
       `(#:include '("\\.el$" "\\.info$" "\\.texi$")))
