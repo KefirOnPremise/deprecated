181e8e6b3eaa403d61797dbf909e4804d503b67e
Adam Kandur <>
2022-07-14 11:46:50 +0300

    wip

--- kefir/pkgs/emacs.scm
+++ kefir/pkgs/emacs.scm
@@ -24,3 +24,27 @@
     (synopsis "my emacs package")
     (description "my emacs package")
     (license license:gpl3+)))
+
+(define-public emacs-sicp
+  ;; named branch is outdated
+  (let ((commit "4002d83083d520c6b5ede2df36cc2cee885d450a")
+        (revision "1"))
+    (package
+     (name "emacs-sicp")
+     (version (git-version "0.0.0" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+	     (url "https://github.com/webframp/sicp-info")
+	     (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+	(base32 "1q2wn1w79gm8pc38x74vi30yyr0b00j9aaaqq13s2pcgbm0ny68d"))))
+     (build-system emacs-build-system)
+     (arguments
+      `(#:include '("\\.el$" "\\.info$" "\\.texi$")))
+     (home-page "https://github.com/webframp/sicp-info")
+     (synopsis "")
+     (description "")
+     (license #f))))
